36091751365ef380ff842542c886717249bfa7a7
Taneli Kaivola <dist+git@negativethinking.net>
2011-04-17 13:43:12 +0300

    Added basic example use of adding items.

--- limuweb/osto/views.py
+++ limuweb/osto/views.py
@@ -3,7 +3,20 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def index(request):
-	data = {}
+	items = request.session.get('items', None)
+
+	if items == None:
+		items = []
+	
+	if request.method == 'POST' and request.POST['inputfield']:
+		if request.POST['inputfield'] == 'CLEAR':
+			items = []
+		else:
+			items.append(request.POST['inputfield'])
+	
+	request.session['items'] = items
+	
+	data = {'items': items[::-1]}
 	return render_to_response('osto/index.html',
 		data,
 		context_instance=RequestContext(request))
