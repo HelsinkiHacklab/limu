d03d9207e4d4068e911141aa29fb90c7424a6681
Taneli Kaivola <taneli.kaivola@nixu.com>
2011-05-17 19:40:57 +0300

    Added MEDIA_URL for urls.

--- limuweb/osto/urls.py
+++ limuweb/osto/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     (r'^$', 'limuweb.osto.views.index'),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
