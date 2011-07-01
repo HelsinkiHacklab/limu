113085935562ee0fcc28cf58247b9337f5176911
Juha Autero <jautero@iki.fi>
2011-07-01 16:12:06 +0300

    Added some basic structure for views.

--- limuweb/tilit/views.py
+++ limuweb/tilit/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from tilit.models import AccountCode
+
+def show(request):
+	account = AccountCode.get_or_code(x)
+	data = {'account': account}
+	return render_to_response('osto/account.html',
+		data,
+		context_instance=RequestContext(request))
+
+def create(request):
+	
